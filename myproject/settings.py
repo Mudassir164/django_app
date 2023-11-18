9ca37e1dff19c86cc3b5e9dc04306d85c6779160
Mudassir Ahmed <52211617+Mudassir164@users.noreply.github.com>
2023-11-18 11:29:54 +0500

    allow all host

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k-=#0iogxax1l6esxrxg_#%r1#_h+ivktqhz05xyw7&u-xxi*d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
